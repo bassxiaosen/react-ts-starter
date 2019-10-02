72e33f3eafa54a5181cee7a38fea9771a575f555
BassXiaoSen <314121378@qq.com>
2019-10-02 20:51:40 +0800

    修改devserver

--- webpack.config.js
+++ webpack.config.js
@@ -112,6 +112,10 @@ module.exports = {
     host: "0.0.0.0", // 可以使用手机访问
     port: 8080,
     historyApiFallback: true, // 该选项的作用所有的404都连接到index.html
+    inline: true,
+    overlay: {
+      errors: true
+    },
     proxy: {
       // 代理到后端的服务地址，会拦截所有以api开头的请求地址
       "/api": "http://localhost:3000"
